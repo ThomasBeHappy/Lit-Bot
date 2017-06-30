5da30bf8a35bce9c18eb994a0defe2287592d0dc
ThomasBeHappy <movecraft.pirates@gmail.com>
2017-06-30 19:13:42 +0200

    fixed welcome messages

--- bot.js
+++ bot.js
@@ -108,8 +108,8 @@ client.on('guildCreate', guild => {
 
 client.on('guildMemberAdd', member => {
 	let guild = member.guild;
-	if (guild.id === 264445053596991498) return;
-	if (guild.id === 110373943822540800) return;
+	if (guild.id === '264445053596991498') return;
+	if (guild.id === '110373943822540800') return;
 	else {
 		guild.defaultChannel.send(`Welcome ${member.user} to this server!`);
 	}
